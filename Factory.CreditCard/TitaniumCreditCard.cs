09263d4f6d06df027b4e52166ab8143086e230de
Santosh Pujari <santosh.pujari@hotmail.com>
2020-09-22 16:47:08 -0500

    FactoryMethod for CreditCard demo

---
+++ Factory.CreditCard/TitaniumCreditCard.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factory.CreditCard
+{
+    class TitaniumCreditCard : CreditCard
+    {
+        private string _cardType;
+
+        public TitaniumCreditCard(int creditLimit, int annualCharge)
+        {
+            this._cardType = "Titanium";
+            this.CreditLimit = creditLimit;
+            this.AnnualCharge = annualCharge;
+        }
+        public override string CardType
+        {
+            get { return _cardType; }
+        }
+    }
+}
