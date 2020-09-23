fd80aa11f888cf432691ea66cff1829e135896b3
Santosh Pujari <santosh.pujari@hotmail.com>
2020-09-23 17:34:29 -0500

    Abstract.Structural Implementation

---
+++ Abstract.Structural/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract.Structural
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AbstractFactory factory1 = new ConcreteFactory1();
+            Client client = new Client(factory1);
+            client.Run();
+
+            AbstractFactory factory2 = new ConcreateFactory2();
+            client = new Client(factory2);
+            client.Run();
+
+            Console.ReadLine();
+        }
+    }
+
+    abstract class AbstractFactory
+    {
+        public abstract AbstractProductA CreateProductA();
+        public abstract AbstractProductB CreateProductB();
+    }
+
+    class ConcreteFactory1 : AbstractFactory
+    {
+        public override AbstractProductA CreateProductA()
+        {
+            return new ProductA1();
+        }
+
+        public override AbstractProductB CreateProductB()
+        {
+            return new ProductB1();
+        }
+    }
+
+    class ConcreateFactory2 : AbstractFactory
+    {
+        public override AbstractProductA CreateProductA()
+        {
+            return new ProductA2();
+        }
+
+        public override AbstractProductB CreateProductB()
+        {
+            return new ProductB2();
+        }
+    }
+
+    abstract class AbstractProductA
+    {
+    }
+    internal class ProductA1 : AbstractProductA
+    {
+    }
+    internal class ProductA2 : AbstractProductA
+    {
+    }
+
+    abstract class AbstractProductB
+    {
+        public abstract void Interact(AbstractProductA a);
+    }
+    internal class ProductB1 : AbstractProductB
+    {
+        public override void Interact(AbstractProductA a)
+        {
+            Console.WriteLine(this.GetType().Name + " interact with " + a.GetType().Name);
+        }
+    }
+    internal class ProductB2 : AbstractProductB
+    {
+        public override void Interact(AbstractProductA a)
+        {
+            Console.WriteLine(this.GetType().Name + " interact with " + a.GetType().Name);
+        }
+    }
+
+    class Client
+    {
+        private AbstractProductA _abstractProductA;
+        private AbstractProductB _abstractProductB;
+
+        public Client(AbstractFactory factory)
+        {
+            _abstractProductA = factory.CreateProductA();
+            _abstractProductB = factory.CreateProductB();
+        }
+
+        public void Run()
+        {
+            _abstractProductB.Interact(_abstractProductA);
+        }
+    }
+}
